d699bdd999713a4059c54d412cc8ea918e656475
Satoshi <6world4trigger@gmail.com>
2024-03-16 06:06:48 +0800

    Add test case for Embedding Layer

--- tests/test_layers.py
+++ tests/test_layers.py
@@ -1,12 +1,9 @@
-import os
 import sys
-print(os.getcwd())
-print(sys.path)
-sys.path.append("C:\\Users\\super\\Documents\\4.Projects\\neural_network\\network_module")
-print(sys.path)
-from initializer import NormalInitializer
+from pathlib import Path
+sys.path.append(str(Path(__file__).parents[0].joinpath('src')))
+print(str(Path(__file__).parents[0].joinpath('src')))
 from layers import Embedding
-
+import numpy as np
 
 def test_embedding():
     vocab_size = 10
@@ -14,16 +11,19 @@ def test_embedding():
     embedding = Embedding(vocab_size, embed_dim)
 
     # test initialize_params
-    embedding.initialize_params(NormalInitializer())
+    embedding.initialize_params()
     assert embedding.embedding.value.shape == (vocab_size, embed_dim)
 
     print(embedding.embedding.value)
 
     # test forward
-    X = [[1, 2, 3], [4, 5, 6]]
+    X = np.array([[1, 2, 3], [4, 5, 6]])
     X_embed = embedding.forward(X)
     assert X_embed.shape == (2, 3, embed_dim)
 
-    # test backward
+    print(X_embed.flatten().shape)
+    print(X.flatten().shape)
+    # test backward (assume X_embed is gradient of loss with respect to X_embed)
     dX = embedding.backward(X_embed)
-    assert dX.shape == (2, 3, vocab_size)
+    print(dX)
+    assert dX.shape == (vocab_size, embed_dim)
